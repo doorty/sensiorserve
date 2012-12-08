1abe4c4c512c4df495e01a5034be46ea74e6131f
Brent Daugherty <brent@doorty.com>
2012-12-08 14:50:47 -0800

    added styles

--- task-model.js
+++ task-model.js
@@ -7,119 +7,28 @@ var mongoose = require('mongoose'),
     MAX_LOGIN_ATTEMPTS = 5,
     LOCK_TIME = 2 * 60 * 60 * 1000;
 
-var UserSchema = new Schema({
-	headline: { type: String, required: true },
-	description: { type: String, required: true },
-	evidence: { type: [String], required: false },
-	hashtag: {type: String, required: false },
-	password: { type: String, required: true },
-	loginAttempts: { type: Number, required: true, default: 0 },
-  lockUntil: { type: Number }
+var TaskSchema = new Schema({
+	userId: { type: String, required: true },
+	description: { type: String, required: false },
+	location: { type: [String], required: false },
+	when: {type: String, required: false },
+	price: { type: String, required: false },
 });
 
-UserSchema.virtual('isLocked').get(function() {
-    // check for a future lockUntil timestamp
-    return !!(this.lockUntil && this.lockUntil > Date.now());
-});
-
-UserSchema.pre('save', function(next) { 
-	var user = this;
+TaskSchema.pre('save', function(next) { 
+	var task = this;
 	
-	// only hash the password if it has been modified (or is new)
-	if (!user.isModified('password')) return next();
-	
-	// generate a salt
-	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-		if (err) return next(err);
 
-		// hash the password along with our new salt
-		bcrypt.hash(user.password, salt, function(err, hash) {
-			if (err) return next(err);
-
-				// override the cleartext password with the hashed one
-				user.password = hash;
-				next();
-		});
-	});
 });
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-		if (err) return cb(err);
-		cb(null, isMatch);
-	});
-};
-
-UserSchema.methods.incLoginAttempts = function(cb) {
-	// if we have a previous lock that has expired, restart at 1
-	if (this.lockUntil && this.lockUntil < Date.now()) {
-	    return this.update({
-	        $set: { loginAttempts: 1 },
-	        $unset: { lockUntil: 1 }
-	    }, cb);
-	}
-	// otherwise we're incrementing
-	var updates = { $inc: { loginAttempts: 1 } };
-	// lock the account if we've reached max attempts and it's not locked already
-	if (this.loginAttempts + 1 >= MAX_LOGIN_ATTEMPTS && !this.isLocked) {
-	    updates.$set = { lockUntil: Date.now() + LOCK_TIME };
-	}
-	return this.update(updates, cb);
-};
-
-UserSchema.statics.getAuthenticated = function(id, password, cb) {
-    this.findOne({ _id: id }, function(err, user) {
-        if (err) return cb(err);
-
-        // make sure the user exists
-        if (!user) {
-            return cb(null, null, reasons.NOT_FOUND);
-        }
-
-        // check if the account is currently locked
-        if (user.isLocked) {
-            // just increment login attempts if account is already locked
-            return user.incLoginAttempts(function(err) {
-                if (err) return cb(err);
-                return cb(null, null, reasons.MAX_ATTEMPTS);
-            });
-        }
-
-        // test for a matching password
-        user.comparePassword(password, function(err, isMatch) {
-            if (err) return cb(err);
-
-            // check if the password was a match
-            if (isMatch) {
-                // if there's no lock or failed attempts, just return the user
-                if (!user.loginAttempts && !user.lockUntil) return cb(null, user);
-                // reset attempts and lock info
-                var updates = {
-                    $set: { loginAttempts: 0 },
-                    $unset: { lockUntil: 1 }
-                };
-                return user.update(updates, function(err) {
-                    if (err) return cb(err);
-                    return cb(null, user);
-                });
-            }
-
-            // password is incorrect, so increment login attempts before responding
-            user.incLoginAttempts(function(err) {
-                if (err) return cb(err);
-                return cb(null, null, reasons.PASSWORD_INCORRECT);
-            });
-        });
-    });
-};
 
 // my convience methods
-UserSchema.methods.action = function(param) {
+TaskSchema.methods.action = function(param) {
 	var result = this.headline + ' with desc ' + this.description;
 	return result;
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', TaskSchema);
 
 
 
