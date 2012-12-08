ff3bad513e1db9dd460aa2705f395ce1bd65615c
Brent Daugherty <brent@doorty.com>
2012-12-08 13:22:08 -0800

    dashboard, create a task pages

--- server.js
+++ server.js
@@ -24,24 +24,18 @@ app.get('/', function(req,res){
   res.render('home.ejs', locals);
 });
 
-app.get('/tasks', function(req,res){
-
+app.get('/dashboard', function(req,res){
   locals = {
     title: 		 'EndAtrocities.com',
     description: '...',
     author: 	 'Brent Daugherty',
     _layoutFile: 'layout.ejs'
   };
-  
-   Enabler.find({ headline: /./i }, function(err, enablers) {
-		console.log("enabler: " + enablers);
-		locals.enablers = enablers;
-		res.render('tasks/listings.ejs', locals);
-	});
 
+  res.render('tasks/dashboard.ejs', locals);
 });
 
-app.get('/report-enabler', function(req,res){
+app.get('/create-task', function(req,res){
   locals = {
     title: 		 'EndAtrocities.com',
     description: '...',
@@ -51,75 +45,6 @@ app.get('/report-enabler', function(req,res){
 
   res.render('tasks/new.ejs', locals);
 });
-
-app.post('/report-enabler', function(req, res, next) {
-
-  console.log("req.body = " + req.body);
-  console.log("req.files = " + req.files);
-  
-  var fileInput, tmp_path, target_path;
-
-  if (req.files.fileEvidence.size === 0) {
-  
-  }
-  else {
-    fileInput = req.files.fileEvidence;
-    // get the temporary location of the file
-    tmp_path = fileInput.path;
-    // set where the file should actually exists - in this case it is in the "evidence" directory
-    target_path = 'public/evidence/' + fileInput.name;
-    // move the file from the temporary location to the intended location
-    fs.rename(tmp_path, target_path, function(err) {
-        if (err) throw err;
-        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-        fs.unlink(tmp_path, function() {
-            if (err) throw err;
-        });
-    });
-  }
-  
-  var enabler = new Enabler({ 
-    headline: req.body.headline,
-    description: req.body.description,
-    evidence: target_path ? [target_path] : [],
-    hashtag: req.body.hashtag ? "#"+req.body.hashtag : "#endatrocity",
-    password: req.body.password
-  });
-  
-	enabler.save(function(err) {
-	  if (err) throw err;
-	  console.log("saved: " + enabler._id);
-	});
-	
-	res.send(JSON.stringify(enabler));
-
-});
-
-app.get('/tasks/:id', function(req,res){
-
-	locals = {
-    title: 		 'EndAtrocities.com',
-    description: '...',
-    author: 	 'Brent Daugherty',
-    enablerId: req.params.id,
-    _layoutFile: 'layout.ejs'
-  };
-
-  Enabler.find({ _id: req.params.id }, function(err, enablers) {
-  	if (err) throw err;
-  	var enabler = enablers[0];
-		console.log("found enabler: " + enabler._id);
-		locals.headline = enabler.headline;
-		locals.description = enabler.description;
-		locals.evidence = enabler.evidence;
-		locals.hashtag = enabler.hashtag;
-		//locals.password = enabler.password;
-		res.render('tasks/view.ejs', locals);
-	}).limit(1);
-
-  
-});
-
 
 /* The 404 Route (ALWAYS Keep this as the last route) */
 app.get('/*', function(req, res){
